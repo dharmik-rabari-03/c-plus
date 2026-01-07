f3a76f1d8fb7023ec7e6a7414a020282d3fac8a1
Dharmik rabari <dharmik161616@gmail.com>
2026-01-07 16:52:18 +0530

    Update q1.cpp

--- ch5-inheritance/ch5.3/q1.cpp
+++ ch5-inheritance/ch5.3/q1.cpp
@@ -1,94 +1,93 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-class Employee
-{
-
-protected:
-    int Employee_id;
-    string emp_name;
-    int age;
-    double salary;
-
-public:
-    void setData(int id, string n, int a, double s)
-    {
-
-        Employee_id = id;
-        emp_name = n;
-        age = a;
-        salary = s;
-    }
-
-    void display()
-    {
-        cout << "Employee Detail :" << endl;
-
-        cout << "ID :" << Employee_id << endl;
-        cout << "NAME :" << emp_name << endl;
-        cout << "AGE :" << age << endl;
-        cout << "SALARY :" << salary << endl;
-    }
-};
-
-class FullTimeEmp : public Employee
-{
-protected:
-    double Bonus;
-
-public:
-
- virtual void FullTimeData(int id,string n,int a,double s,double b){
-
-   Employee::setData(id,n,a,s);
-
-    Bonus=b;
-
- }
-
- virtual void display(){
-
-        cout<<"Full Time Employee "<<endl;
-        Employee::display();
-        cout<<"Bonus :"<<Bonus<<endl;
- }
-
-
-};
-
-class PartTimeEmp : public Employee
-{
-protected:
-   float hour;
-
-public:
-
- virtual void PartTimeData(int id,string n,int a,double s,float h){
-
-   Employee::setData(id,n,a,s);
-    hour=h;
-   
-
- }
-
- virtual void display(){
-
-        cout<<"part Time Employee "<<endl;
-        Employee::display();
-        cout<<"hour :"<<hour<<endl;
- }
-
-
-};
-
 int main()
 {
+    const int MAX = 10;
+    Employee* emp[MAX];   // array of pointer objects
+    int count = 0;
+    int choice;
 
-    PartTimeEmp p;
-    p.PartTimeData(101, "Rahul", 22, 15000, 6.5);
-    p.display();
+    do
+    {
+        cout << "\n===== Employee Management System =====" << endl;
+        cout << "1. Add Full Time Employee" << endl;
+        cout << "2. Add Part Time Employee" << endl;
+        cout << "3. Display All Employees" << endl;
+        cout << "4. Delete Last Employee" << endl;
+        cout << "5. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
 
+        if (choice == 1 && count < MAX)
+        {
+            int id, age;
+            double salary, bonus;
+            string name;
 
+            cout << "Enter ID: ";
+            cin >> id;
+            cout << "Enter Name: ";
+            cin >> name;
+            cout << "Enter Age: ";
+            cin >> age;
+            cout << "Enter Salary: ";
+            cin >> salary;
+            cout << "Enter Bonus: ";
+            cin >> bonus;
+
+            FullTimeEmployee* fte = new FullTimeEmployee();
+            fte->setFullTimeEmployee(id, name, age, salary, bonus);
+            emp[count++] = fte;
+        }
+        else if (choice == 2 && count < MAX)
+        {
+            int id, age;
+            double salary;
+            float hours;
+            string name;
+
+            cout << "Enter ID: ";
+            cin >> id;
+            cout << "Enter Name: ";
+            cin >> name;
+            cout << "Enter Age: ";
+            cin >> age;
+            cout << "Enter Salary: ";
+            cin >> salary;
+            cout << "Enter Hours Worked: ";
+            cin >> hours;
+
+            PartTimeEmployee* pte = new PartTimeEmployee();
+            pte->setPartTimeEmployee(id, name, age, salary, hours);
+            emp[count++] = pte;
+        }
+        else if (choice == 3)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                emp[i]->display(); // polymorphism
+            }
+        }
+        else if (choice == 4)
+        {
+            if (count > 0)
+            {
+                delete emp[count - 1]; // free memory
+                count--;
+                cout << "Employee deleted successfully." << endl;
+            }
+            else
+            {
+                cout << "No employee to delete." << endl;
+            }
+        }
+
+    } while (choice != 5);
+
+    // Free all remaining memory
+    for (int i = 0; i < count; i++)
+    {
+        delete emp[i];
+    }
+
+    cout << "Program exited. Memory freed." << endl;
     return 0;
 }
