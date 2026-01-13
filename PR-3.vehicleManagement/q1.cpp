8c0a3897afbe2842cae344f19fa467ce8de29b58
Dharmik rabari <dharmik161616@gmail.com>
2026-01-13 14:54:32 +0530

    Add files via upload

---
+++ PR-3.vehicleManagement/q1.cpp
@@ -0,0 +1,354 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+class Vehicle
+{
+protected:
+    int vehicleID;
+    string manufacturer;
+    string model;
+    int year;
+
+public:
+    static int totalVehicles;
+
+    Vehicle()
+    {
+        vehicleID = 0;
+        year = 0;
+    }
+
+    Vehicle(int id, string manu, string mod, int yr)
+    {
+        vehicleID = id;
+        manufacturer = manu;
+        model = mod;
+        year = yr;
+        totalVehicles++;
+    }
+
+    virtual ~Vehicle() {}
+
+ 
+    void setVehicleID(int id)
+    {
+        if (id > 0)
+            vehicleID = id;
+    }
+
+   
+    int getVehicleID()
+    {
+        return vehicleID;
+    }
+
+    void setManufacturer(string m)
+    {
+        manufacturer = m;
+    }
+
+    string getManufacturer()
+    {
+        return manufacturer;
+    }
+
+    virtual void display() const
+    {
+        cout << "ID             : " << vehicleID
+             << "Manufacturer   : " << manufacturer
+             << "Model          : " << model
+             << "Year           : " << year;
+    }
+};
+
+int Vehicle::totalVehicles = 0;
+
+
+
+class Car : public Vehicle
+{
+protected:
+    string fuelType;
+
+public:
+    Car() : Vehicle() {}
+
+    Car(int id, string manu, string mod, int yr, string fuel)
+        : Vehicle(id, manu, mod, yr), fuelType(fuel) {}
+
+    void display() const override
+    {
+        Vehicle::display();
+        cout << "Fuel Type: " << fuelType;
+    }
+};
+
+
+
+class ElectricCar : public Car
+{
+protected:
+    int batteryCapacity;
+
+public:
+    ElectricCar(int id, string manu, string mod, int yr, string fuel, int battery)
+        : Car(id, manu, mod, yr, fuel), batteryCapacity(battery) {}
+
+    void display() const override
+    {
+        Car::display();
+        cout << ", Battery: " << batteryCapacity << " kWh";
+    }
+};
+
+class SportsCar : public ElectricCar
+{
+    int topSpeed;
+
+public:
+    SportsCar(int id, string manu, string mod, int yr, string fuel, int battery, int speed)
+        : ElectricCar(id, manu, mod, yr, fuel, battery), topSpeed(speed) {}
+
+    void display() const override
+    {
+        ElectricCar::display();
+        cout << "Top Speed: " << topSpeed << " km/h";
+    }
+};
+
+class Sedan : public Car
+{
+public:
+    Sedan(int id, string manu, string mod, int yr, string fuel)
+        : Car(id, manu, mod, yr, fuel) {}
+
+    void display() const override
+    {
+        cout << "[Sedan] ";
+        Car::display();
+    }
+};
+
+class SUV : public Car
+{
+public:
+    SUV(int id, string manu, string mod, int yr, string fuel)
+        : Car(id, manu, mod, yr, fuel) {}
+
+    void display() const override
+    {
+        cout << "[SUV] ";
+        Car::display();
+    }
+};
+
+
+class Aircraft
+{
+protected:
+    int flightRange;
+
+public:
+    Aircraft(int range = 0)
+    {
+        flightRange = range;
+    }
+};
+
+class FlyingCar : public Car, public Aircraft
+{
+public:
+    FlyingCar(int id, string manu, string mod, int yr, string fuel, int range)
+        : Car(id, manu, mod, yr, fuel), Aircraft(range) {}
+
+    void display() const
+    {
+        cout << "[FlyingCar] ID: " << vehicleID
+             << ", Manufacturer: " << manufacturer
+             << ", Model: " << model
+             << ", Year: " << year
+             << ", Fuel: " << fuelType
+             << ", Flight Range: " << flightRange << " km";
+    }
+};
+
+
+class VehicleRegistry
+{
+    Vehicle *vehicles[50];
+    int count;
+
+public:
+    VehicleRegistry()
+    {
+        count = 0;
+    }
+
+    void addVehicle(Vehicle *v)
+    {
+        if (count < 50)
+        {
+            vehicles[count++] = v;
+            cout << "Vehicle added successfully!\n";
+        }
+        else
+        {
+            cout << "Registry is full!\n";
+        }
+    }
+
+    void displayAll() const
+    {
+        if (count == 0)
+        {
+            cout << "No vehicles found.\n";
+            return;
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            vehicles[i]->display();
+            cout << endl;
+        }
+    }
+
+    void searchByID(int id) const
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if (vehicles[i]->getVehicleID() == id)
+            {
+                vehicles[i]->display();
+                cout << endl;
+                return;
+            }
+        }
+        cout << "Vehicle not found!\n";
+    }
+};
+
+
+int main()
+{
+    VehicleRegistry registry;
+    int choice;
+
+    do
+    {
+        cout << "\n--- Vehicle Registry System ---\n";
+        cout << "1. Add Sedan\n";
+        cout << "2. Add SUV\n";
+        cout << "3. Add Sports Car\n";
+        cout << "4. Add Flying Car\n";
+        cout << "5. View All Vehicles\n";
+        cout << "6. Search by ID\n";
+        cout << "0. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        int id, year, battery, speed, range;
+        string manu, model, fuel;
+
+       switch (choice)
+{
+case 1:
+    cout << "\n--- Add Sedan ---\n";
+    cout << "Enter Vehicle ID        : ";
+    cin >> id;
+    cout << "Enter Manufacturer      : ";
+    cin >> manu;
+    cout << "Enter Model             : ";
+    cin >> model;
+    cout << "Enter Year              : ";
+    cin >> year;
+    cout << "Enter Fuel Type         : ";
+    cin >> fuel;
+
+    registry.addVehicle(new Sedan(id, manu, model, year, fuel));
+    break;
+
+case 2:
+    cout << "\n--- Add SUV ---\n";
+    cout << "Enter Vehicle ID        : ";
+    cin >> id;
+    cout << "Enter Manufacturer      : ";
+    cin >> manu;
+    cout << "Enter Model             : ";
+    cin >> model;
+    cout << "Enter Year              : ";
+    cin >> year;
+    cout << "Enter Fuel Type         : ";
+    cin >> fuel;
+
+    registry.addVehicle(new SUV(id, manu, model, year, fuel));
+    break;
+
+case 3:
+    cout << "\n--- Add Sports Car ---\n";
+    cout << "Enter Vehicle ID        : ";
+    cin >> id;
+    cout << "Enter Manufacturer      : ";
+    cin >> manu;
+    cout << "Enter Model             : ";
+    cin >> model;
+    cout << "Enter Year              : ";
+    cin >> year;
+    cout << "Enter Fuel Type         : ";
+    cin >> fuel;
+    cout << "Enter Battery Capacity  : ";
+    cin >> battery;
+    cout << "Enter Top Speed         : ";
+    cin >> speed;
+
+    registry.addVehicle(
+        new SportsCar(id, manu, model, year, fuel, battery, speed));
+    break;
+
+case 4:
+    cout << "\n--- Add Flying Car ---\n";
+    cout << "Enter Vehicle ID        : ";
+    cin >> id;
+    cout << "Enter Manufacturer      : ";
+    cin >> manu;
+    cout << "Enter Model             : ";
+    cin >> model;
+    cout << "Enter Year              : ";
+    cin >> year;
+    cout << "Enter Fuel Type         : ";
+    cin >> fuel;
+    cout << "Enter Flight Range      : ";
+    cin >> range;
+
+    registry.addVehicle(
+        (Vehicle *)new FlyingCar(id, manu, model, year, fuel, range));
+    break;
+
+case 5:
+    cout << "\n--- All Registered Vehicles ---\n";
+    registry.displayAll();
+    break;
+
+case 6:
+    cout << "\n--- Search Vehicle ---\n";
+    cout << "Enter Vehicle ID        : ";
+    cin >> id;
+    registry.searchByID(id);
+    break;
+
+case 7:
+    cout << "\nExiting Vehicle Registry System...\n";
+    break;
+
+default:
+    cout << "\nInvalid choice! Please try again.\n";
+}
+
+
+    } while (choice != 7);
+
+    cout << "Total Vehicles Created: " << Vehicle::totalVehicles << endl;
+    return 0;
+}
